69351f50a859e33bb6f79c71f1274ac63585620b
Havok <rngdhavok@hotmail.com>
2023-01-20 01:00:59 -0500

    Work on API function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
-var cityName = document.querySelector(".city-name");
-var apiKey = "817bdcdf8036c1ecbb4e09f9a1e15684";
+var cityName = document.querySelector(".btn");
 
 console.log(cityName);
 
 
 function getApi() {
-    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+ cityName +"&appid="+ apiKey;
+    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+ cityName +"&appid=817bdcdf8036c1ecbb4e09f9a1e15684";
   
     fetch(requestUrl)
       .then(function (response) {
         return response.json();
+        
       })
       .then(function (data) {
         for (var i = 0; i < data.length; i++) {
@@ -18,4 +18,7 @@ function getApi() {
           repoList.appendChild(listItem);
         }
       });
+
+      console.log(data);
   }
+
