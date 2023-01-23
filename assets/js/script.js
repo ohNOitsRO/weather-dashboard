f3947c8681d8d010bfe1b86bfeff89f23ff8abb8
Havok <rngdhavok@hotmail.com>
2023-01-23 00:26:33 -0500

    WIP

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,21 @@ $("#searchBtn").click(function(){
   var cityName = $(this).siblings(".form-control").val();
   localStorage.setItem(cityName,cityName);
   console.log(cityName);
+  getApi();
 })
 
 
 function getApi() {
-    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+ cityName +"&appid=e199a596c22e40837091b277980ac2a5";
+    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+ cityName +"&appid=03e2b0141cd0f7d9d15a27103279bb3e&units=metric";
     
     fetch(requestUrl)
       .then(function (response) {
         return response.json();
-        
+       
       })
+
       .then(function (data) {
+        console.log(data);
         for (var i = 0; i < data.length; i++) {
           var listItem = document.createElement('li');
           listItem.textContent = data[i].html_url;
@@ -26,6 +29,7 @@ function getApi() {
         }
       });
 
-      console.log(data);
+
+      
   }
 
