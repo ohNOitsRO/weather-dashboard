9b1a88a5175761d471c08c3d274fae4407a17c96
Delgado <rxd718@miami.edu>
2023-01-23 12:17:04 -0500

    WIP

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,27 @@
-var cityName; 
+var todayWeather = document.querySelector("#current-weather");
+var cityName = "";
 
 // https://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&appid=e199a596c22e40837091b277980ac2a5
 // FOR DAILY WEATHER CALLS
 
 $("#searchBtn").click(function(){
   var cityName = $(this).siblings(".form-control").val();
-  localStorage.setItem(cityName,cityName);
+  localStorage.setItem("search",cityName);
   console.log(cityName);
   getApi();
 })
 
 
 function getApi() {
-    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+ cityName +"&appid=03e2b0141cd0f7d9d15a27103279bb3e&units=metric";
+    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid=03e2b0141cd0f7d9d15a27103279bb3e&units=metric";
     
     fetch(requestUrl)
       .then(function (response) {
+        console.log(response);
+        if (response.status === 200) {
+          responseText.textContent = response.status;
+        }
         return response.json();
-       
       })
 
-      .then(function (data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-          var listItem = document.createElement('li');
-          listItem.textContent = data[i].html_url;
-          repoList.appendChild(listItem);
-        }
-      });
-
-
-      
-  }
-
+      }
