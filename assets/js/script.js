11cd1ccbb39545424510eaed1813e4715f261057
Delgado <rxd718@miami.edu>
2023-01-23 15:21:30 -0500

    WIP

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,34 @@
 var todayWeather = document.querySelector("#current-weather");
-var cityName = "";
+var cityName;
 
 // https://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&appid=e199a596c22e40837091b277980ac2a5
 // FOR DAILY WEATHER CALLS
 
-$("#searchBtn").click(function(){
+$("#searchBtn").click(function(event){
+  event.preventDefault();
   var cityName = $(this).siblings(".form-control").val();
   localStorage.setItem("search",cityName);
   console.log(cityName);
   getApi();
-})
+
 
 
 function getApi() {
-    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid=03e2b0141cd0f7d9d15a27103279bb3e&units=metric";
+    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=03e2b0141cd0f7d9d15a27103279bb3e&units=metric";
     
     fetch(requestUrl)
       .then(function (response) {
         console.log(response);
-        if (response.status === 200) {
-          responseText.textContent = response.status;
-        }
         return response.json();
       })
+      .then (function(data) {
+         todayWeather.textContent = data[0].name + " (" + moment().format("M/D/YYYY") + ") ";  
+         console.log(data);
+
 
       }
+      )
+
+      }
+
+    })
